776d20dbfb8f024023061b90605e2399850e67b2
Ananda Braga <anandabragal@gmail.com>
2023-08-01 22:32:13 -0300

    010823

--- src/pages/bemVindo/index.jsx
+++ src/pages/bemVindo/index.jsx
@@ -1,22 +1,24 @@
-import styles from './style.css'
-import Logo from '../../img/logoYouOut.jpeg'
+import styles from "./style.css";
+import Logo from "../../img/logoYouOut.jpeg";
 
 function BemVindo() {
-    return(
-        <div className='divPrincipal'>
-             <div className='divLogo'>
-                <img src={Logo} id='logo' alt='Logo YouOut'/> 
-            </div>
-            
-            <div className='btns'>
-                <h1 id='titulo'>Bem-Vindo ao <br></br>YouOut!</h1>
-                <button className='btnUsuario'>USUÁRIO</button>
-                <button className='btnEstabelecimento'>ESTABELECIMENTO</button>
-                <button className='btnCadastrese'>CADASTRE-SE</button>
-            </div>
+  return (
+    <div className="divPrincipal">
+      <div className="divLogo">
+        <img src={Logo} id="logo" alt="Logo YouOut" />
+      </div>
+      <div className="divButtons">
+        {/* <div className="btn"> */}
+          <h1 id="titulo">
+            Bem-Vindo ao <br></br>YouOut!
+          </h1>
+          <button className="btnUsuario">USUÁRIO</button>
+          <button className="btnEstabelecimento">ESTABELECIMENTO</button>
+          <button className="btnCadastrese">CADASTRE-SE</button>
         </div>
-
-    )
+      {/* </div> */}
+    </div>
+  );
 }
 
 export default BemVindo;
