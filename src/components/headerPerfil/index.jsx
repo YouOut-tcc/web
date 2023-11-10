1e1700479d253d93bde475952cdbc5472400f6fa
Jaqueline Alencar <111159660+Jaquelineralencar@users.noreply.github.com>
2023-11-10 12:08:21 -0300

    ajuste eventos

--- src/components/headerPerfil/index.jsx
+++ src/components/headerPerfil/index.jsx
@@ -1,38 +1,45 @@
-import styles from '../headerPerfil/style.css';
-import CommerceLogo from '../../img/commerceLogo.png';
-import React, { useState } from 'react';
+import styles from "../headerPerfil/style.css";
+import CommerceLogo from "../../img/commerceLogo.png";
+import React, { useState } from "react";
 
 export default function HeaderPerfil() {
-  
-const estabelecimentos = [
-  { id: 1, nome: 'Estabelecimento 1', imagem: CommerceLogo },
-  { id: 2, nome: 'Estabelecimento 2', imagem: CommerceLogo },
-  { id: 3, nome: 'Estabelecimento 3', imagem: CommerceLogo },
-];
-  const [selectedEstabelecimento, setSelectedEstabelecimento] = useState(estabelecimentos[0]);
+  const estabelecimentos = [
+    { id: 1, nome: "Estabelecimento 1", imagem: CommerceLogo },
+    { id: 2, nome: "Estabelecimento 2", imagem: CommerceLogo },
+    { id: 3, nome: "Estabelecimento 3", imagem: CommerceLogo },
+  ];
+  const [selectedEstabelecimento, setSelectedEstabelecimento] = useState(
+    estabelecimentos[0]
+  );
 
   const handleEstabelecimentoChange = (event) => {
     const estabelecimentoId = parseInt(event.target.value);
-    const selected = estabelecimentos.find((estabelecimento) => estabelecimento.id === estabelecimentoId);
+    const selected = estabelecimentos.find(
+      (estabelecimento) => estabelecimento.id === estabelecimentoId
+    );
     setSelectedEstabelecimento(selected);
   };
-  return(
-      <>
-        <div className='HPerfil'>
-        <select value={selectedEstabelecimento.id} onChange={handleEstabelecimentoChange} className='unidade'>
-          {estabelecimentos.map((estabelecimento) => (
-            <option key={estabelecimento.id} value={estabelecimento.id}>
-              {estabelecimento.nome}
-            </option>
-          ))}
-        </select>
-        <div className="estabelecimento-info">
-          <img src={selectedEstabelecimento.imagem} alt={selectedEstabelecimento.nome} className='fotoPerfilCommerce'/>
-          <span className='nome'>{selectedEstabelecimento.nome}</span>
-        </div>
+  return (
+    <div className="HPerfil">
+      <select
+        value={selectedEstabelecimento.id}
+        onChange={handleEstabelecimentoChange}
+        className="unidade"
+      >
+        {estabelecimentos.map((estabelecimento) => (
+          <option key={estabelecimento.id} value={estabelecimento.id}>
+            {estabelecimento.nome}
+          </option>
+        ))}
+      </select>
+      <div className="estabelecimento-info">
+        <img
+          src={selectedEstabelecimento.imagem}
+          alt={selectedEstabelecimento.nome}
+          className="fotoPerfilCommerce"
+        />
+        <span className="nome">{selectedEstabelecimento.nome}</span>
       </div>
-        
-      </>
-
-  )
+    </div>
+  );
 }
--- src/pages/pagEventos/index.jsx
+++ src/pages/pagEventos/index.jsx
@@ -1,60 +1,68 @@
-import styles from '../pagEventos/style.css';
-import Header from '../../components/Header';
-import HeaderPerfil from '../../components/headerPerfil';
-import {BsCalendarCheck} from 'react-icons/bs'
-import {BsPlusCircleFill} from 'react-icons/bs'
-import {useState} from 'react';
-import Modal from '../../components/modalCupons';
-import {BiTimeFive} from 'react-icons/bi'
-import {RiMoneyDollarCircleFill} from 'react-icons/ri'
-import CommerceLogo from '../../img/commerceLogo.png';
-import { Link } from 'react-router-dom';
-import CardEventos from '../../components/cardEventos/cardEvento';
+import styles from "../pagEventos/style.css";
+import Header from "../../components/Header";
+import HeaderPerfil from "../../components/headerPerfil";
+import { BsCalendarCheck } from "react-icons/bs";
+import { BsPlusCircleFill } from "react-icons/bs";
+import { useState } from "react";
+import Modal from "../../components/modalCupons";
+import { BiTimeFive } from "react-icons/bi";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import CommerceLogo from "../../img/commerceLogo.png";
+import { Link } from "react-router-dom";
+import CardEventos from "../../components/cardEventos/cardEvento";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import { BiEdit } from "react-icons/bi";
 
 function PagEventos() {
-  const [openModal, setOpenModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
 
-  return(
-      <div className='containerEventos'>
-          {/* <Header/> */}
-          <HeaderPerfil/>
-          <div className='divTituloEvento'>
-            <h1 className='tituloHeaderEvento' > Eventos</h1>   
-            <BsCalendarCheck className='iconeEvento'/>
-            <button className='btnAddEvento' onClick={() => setOpenModal(true)}><BsPlusCircleFill className='iconeAddEvento'/></button>
-          </div>
-          <div className='divPaiModalConteudoEventos'>
-            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
-              <div className='divModalConteudoEventos'>
-
-                <h1 className='tituloEvento'>Evento De Fulano</h1>
-
-                <div className='divModalConteudoImgEventos'>
-
-                <img src={CommerceLogo}  className='imgLogoEvento'/>
-
-                </div>
-
-                <div className='divModalConteudoInputsEventos'>
-                  <h1>Detalhes</h1>
-                  <BsCalendarCheck/> <input type="text" id='inicio' />
-                  <BiTimeFive/><input type="text" id='fim'/>
-                  <RiMoneyDollarCircleFill/> <input type="text" id='descricao' />
-                  <button className='btnSalvarEvento'>Salvar Evento</button>
-                 
-                </div>
-                
-              </div>
-            </Modal>
-          </div>
-
-            <div className='evento'>
-             
-           <CardEventos/>
-            </div>
-        
+  return (
+    <div className="containerEventos">
+      <HeaderPerfil/>
+      <div className="divTituloEvento">
+        <h1><BsCalendarCheck/> Eventos</h1>
+        <Box
+          sx={{
+            "& > :not(style)": { m: 1 },
+            justifyContent: "end",
+            height: "auto",
+            display: "flex",
+            marginTop: "-4vh"
+          }}
+        >
+          <Fab color="primary" aria-label="add" style={{width: "50px", height: "50px"}} onClick={() => setOpenModal(true)}>
+            <BsPlusCircleFill size={25}  />
+          </Fab>
+        </Box>
       </div>
-  )
-  }
-  
-  export default PagEventos;
+      <div className="divPaiModalConteudoEventos">
+        <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
+          <div className="divModalConteudoEventos">
+            <h1 className="tituloEvento">Evento De Fulano</h1>
+
+            <div className="divModalConteudoImgEventos">
+              <img src={CommerceLogo} className="imgLogoEvento" />
+            </div>
+
+            <div className="divModalConteudoInputsEventos">
+              <h1>Detalhes</h1>
+              <BsCalendarCheck /> <input type="text" id="inicio" />
+              <BiTimeFive />
+              <input type="text" id="fim" />
+              <RiMoneyDollarCircleFill /> <input type="text" id="descricao" />
+              <button className="btnSalvarEvento">Salvar Evento</button>
+            </div>
+          </div>
+        </Modal>
+      </div>
+
+      <div className="evento">
+        <CardEventos />
+      </div>
+    </div>
+  );
+}
+
+export default PagEventos;
